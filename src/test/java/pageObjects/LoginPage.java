84ecf64e0c1f7a3902571e0ad40ec399d283473b
Maya Babuji <mayababuji@outlook.com>
2025-11-06 07:19:47 -0500

    added exception

--- src/test/java/pageObjects/LoginPage.java
+++ src/test/java/pageObjects/LoginPage.java
@@ -1,28 +1,5 @@
 package pageObjects;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-
-import utilities.ElementUtil;
-import utilities.ReadConfig;
-
 public class LoginPage {
-	private WebDriver driver;
-	private ElementUtil util;
-	private ReadConfig readConfig;
-	private By menstrualPhase = By.xpath("//dummy");
-	public LoginPage(WebDriver driver) {
-		this.driver = driver;
-		util = new ElementUtil(this.driver);
-		readConfig = new ReadConfig();
-		
-	}
-	 public String getMenstrualText() {
-		 return util.getElementText(menstrualPhase);
-	       
-	    }
-	 public boolean isMenstrualPhaseDisplayed() {
-		 return util.isElementDisplayed(menstrualPhase);
-	       
-	    }
+
 }
